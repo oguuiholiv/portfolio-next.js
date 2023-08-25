2d02a2f57e2b2b8bef0ab928c8b67c8f2ed02097
oguuiholiv <francoguilherme.sec@gmail.com>
2023-08-25 14:34:19 -0300

    :bug: -> fix: Error loading external fonts fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,24 @@ import About from "../src/blocks/About";
 import Presentation from "../src/blocks/Presentation";
 import Service from "../src/blocks/Service";
 import Navbar from "../src/components/Navbar";
-
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+   
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Orbitron&family=Ubuntu&family=Wavefont:wght@200&display=swap"
+          
+        />
+      
+
       <Navbar />
       <Presentation />
       <About />
       <Service />
     </div>
-  )
+  );
 }
